4e69c4ab23997981009bdf7140a22d21e815f0c3
Adam <adam@wednesplace.com>
2025-02-25 13:31:13 +0100

    update versions

--- exoclick/exoclick.go
+++ exoclick/exoclick.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	Version          = "v0.0.2"
+	Version          = "v0.0.3"
 	defaultBaseUrl   = "https://api.exoclick.com/v2/"
 	defaultUserAgent = "go-exoclick" + "/" + Version
 
